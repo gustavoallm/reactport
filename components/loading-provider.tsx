add0b68fe7190eb66efd4902ad74c86c2a68fba5
Gustavo Carvalho <gustavo_allm@outlook.com>
2026-01-09 00:47:25 -0300

    feat(loading): improve loading time and rename files

--- components/loading-provider.tsx
+++ components/loading-provider.tsx
@@ -1,7 +1,7 @@
 "use client";
 
 import { useState } from "react";
-import { Loading } from "./Loading";
+import { Loading } from "./loading";
 
 interface LoadingProviderProps {
   children: React.ReactNode;
